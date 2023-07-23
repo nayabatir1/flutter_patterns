25ff0f7990750167b2d4a036fcc64f2cc1b361e1
Atir Nayab <atirnayab@Atirs-MacBook-Air.local>
2023-07-24 01:09:33 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,93 @@
 import 'package:flutter/material.dart';
 
+import 'data.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const DocumentApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+        theme: ThemeData(useMaterial3: true),
+        home: DocumentScreen(
+          document: Document(),
+        ));
+  }
+}
+
+String formatDate(DateTime dateTime) {
+  final today = DateTime.now();
+  final difference = dateTime.difference(today);
+
+  return switch (difference) {
+    Duration(inDays: 0) => 'today',
+    Duration(inDays: 1) => 'tomorrow',
+    Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days) when days > 7 => '${days ~/ 7} weeks from now',
+    Duration(inDays: final days) when days < -7 =>
+      '${days.abs() ~/ 7} week ago',
+    Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
+    Duration(inDays: final days) => '$days days from now',
+  };
+}
+
+class DocumentScreen extends StatelessWidget {
+  const DocumentScreen({super.key, required this.document});
+
+  final Document document;
+
+  @override
+  Widget build(BuildContext context) {
+    final (:modified, title) = document.metadata;
+    final blocks = document.getBlocks();
+    final formattedModifiedDate = formatDate(modified);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Text('Last modified $formattedModifiedDate'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({super.key, required this.block});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.all(8),
+        child: switch (block) {
+          HeaderBlock(:final text) => Text(
+              text,
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ParagraphBlock(:final text) => Text(text),
+          CheckboxBlock(:final text, :final isChecked) => Row(
+              children: [
+                Checkbox(value: isChecked, onChanged: (_) {}),
+                Text(text),
+              ],
+            ),
+        });
   }
 }
